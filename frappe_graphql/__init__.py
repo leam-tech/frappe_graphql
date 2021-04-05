98dab11a59c49c37e459c5da7a0d821a4583fb11
Fahim Ali Zain <faztp12@gmail.com>
2021-04-05 11:11:11 +0530

    feat: Plug N Play Pagination

--- frappe_graphql/__init__.py
+++ frappe_graphql/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from .utils.cursor_pagination import CursorPaginator  # noqa
 
 __version__ = '0.0.1'
--- frappe_graphql/utils/resolver/__init__.py
+++ frappe_graphql/utils/resolver/__init__.py
@@ -3,7 +3,7 @@ from graphql import GraphQLObjectType, GraphQLResolveInfo
 
 import frappe
 
-from .list_resolver import list_resolver
+from frappe_graphql import CursorPaginator
 from .document_resolver import document_resolver
 from .utils import get_singular_doctype, get_plural_doctype
 
@@ -25,12 +25,8 @@ def default_field_resolver(obj: Any, info: GraphQLResolveInfo, **kwargs):
 
         plural_doctype = get_plural_doctype(info.field_name)
         if plural_doctype:
-            kwargs["doctype"] = plural_doctype
-            return list_resolver(
-                obj=obj,
-                info=info,
-                **kwargs
-            )
+            frappe.has_permission(doctype=plural_doctype, throw=True)
+            return CursorPaginator(doctype=plural_doctype).resolve(obj, info, **kwargs)
 
     if not isinstance(obj, dict):
         return None
@@ -40,7 +36,10 @@ def default_field_resolver(obj: Any, info: GraphQLResolveInfo, **kwargs):
     # - these form the resolvers for
     #   "SET_VALUE_TYPE", "SAVE_DOC_TYPE", "DELETE_DOC_TYPE" mutations
     if info.field_name in obj:
-        return obj.get(info.field_name)
+        value = obj.get(info.field_name)
+        if isinstance(value, CursorPaginator):
+            return value.resolve(obj, info, **kwargs)
+        return value
 
     if obj.get("name") and (obj.get("doctype") or get_singular_doctype(parent_type.name)):
         # this section is executed for Fields on DocType object types.
