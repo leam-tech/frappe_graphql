ef06e2b877442ba32a3ce42b88e40e01b61eadd3
Fahim Ali Zain <faztp12@gmail.com>
2021-05-29 15:03:46 +0530

    feat: Requires Roles Wrapper

--- frappe_graphql/__init__.py
+++ frappe_graphql/__init__.py
@@ -2,5 +2,6 @@
 from __future__ import unicode_literals
 from .utils.cursor_pagination import CursorPaginator  # noqa
 from .utils.exceptions import ERROR_CODED_EXCEPTIONS, GQLExecutionUserError, GQLExecutionUserErrorMultiple  # noqa
+from .utils.roles import REQUIRE_ROLES  # noqa
 
 __version__ = '1.0.0'
