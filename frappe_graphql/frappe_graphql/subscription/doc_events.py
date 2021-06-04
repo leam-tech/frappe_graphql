b0d56e9a6dd549aacc1a611ef2b2338808221c06
Fahim Ali Zain <faztp12@gmail.com>
2021-06-04 09:04:08 +0530

    docs: Client side example code for subscriptions in vanilla-js fix: optimized doc_events fix: restrict doc_events for Guests fix: notify_consumers - new subscription utility function

--- frappe_graphql/frappe_graphql/subscription/doc_events.py
+++ frappe_graphql/frappe_graphql/subscription/doc_events.py
@@ -1,6 +1,6 @@
 import frappe
 from graphql import GraphQLSchema, GraphQLResolveInfo
-from frappe_graphql import setup_subscription, get_consumers, notify_consumer, get_schema
+from frappe_graphql import setup_subscription, get_consumers, notify_consumers, get_schema
 from frappe_graphql.utils.resolver import get_singular_doctype
 
 
@@ -9,6 +9,9 @@ def bind(schema: GraphQLSchema):
 
 
 def doc_events_resolver(obj, info: GraphQLResolveInfo, **kwargs):
+    if frappe.session.user == "Guest":
+        frappe.throw("Not Allowed for Guests")
+
     return setup_subscription(
         subscription="doc_events",
         info=info,
@@ -19,38 +22,35 @@ def doc_events_resolver(obj, info: GraphQLResolveInfo, **kwargs):
 def on_change(doc, method=None):
     if frappe.flags.in_migrate:
         return
+    # Verify DocType type has beed defined in SDL
+    schema = get_schema()
+    if not schema.get_type(get_singular_doctype(doc.doctype)):
+        return
+
+    subscription_ids = []
+    for consumer in get_consumers("doc_events"):
+        doctypes = consumer.variables.get("doctypes", [])
+        if len(doctypes) and doc.doctype not in doctypes:
+            continue
+
+        subscription_ids.append(consumer.subscription_id)
+
     frappe.enqueue(
         notify_consumers,
         enqueue_after_commit=True,
-        doctype=doc.doctype,
-        name=doc.name,
-        triggered_by=frappe.session.user
+        subscription="doc_events",
+        subscription_ids=subscription_ids,
+        data=frappe._dict(
+            event="on_change",
+            doctype=doc.doctype,
+            name=doc.name,
+            document=frappe._dict(
+                doctype=doc.doctype,
+                name=doc.name
+            ),
+            triggered_by=frappe._dict(
+                doctype="User",
+                name=frappe.session.user
+            )
+        )
     )
-
-
-def notify_consumers(doctype, name, triggered_by):
-    # Verify DocType type has beed defined in SDL
-    schema = get_schema()
-    if not schema.get_type(get_singular_doctype(doctype)):
-        return
-
-    for consumer in get_consumers("doc_events"):
-        doctypes = consumer.variables.get("doctypes", [])
-        if len(doctypes) and doctype not in doctypes:
-            continue
-        notify_consumer(
-            subscription="doc_events",
-            subscription_id=consumer.subscription_id,
-            data=frappe._dict(
-                event="on_change",
-                doctype=doctype,
-                name=name,
-                document=frappe._dict(
-                    doctype=doctype,
-                    name=name
-                ),
-                triggered_by=frappe._dict(
-                    doctype="User",
-                    name=triggered_by
-                )
-            ))
