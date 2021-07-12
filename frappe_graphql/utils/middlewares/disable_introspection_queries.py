36de979521561851d041aab71a319f57bbfe8637
Fahim Ali Zain <fahimalizain@gmail.com>
2021-07-12 07:52:48 +0530

    fix: translation fix: cint check docs: updates

--- frappe_graphql/utils/middlewares/disable_introspection_queries.py
+++ frappe_graphql/utils/middlewares/disable_introspection_queries.py
@@ -1,4 +1,5 @@
 import frappe
+from frappe.utils import cint
 from graphql import GraphQLResolveInfo
 
 
@@ -9,11 +10,11 @@ class IntrospectionDisabled(Exception):
 def disable_introspection_queries(next_resolver, obj, info: GraphQLResolveInfo, **kwargs):
     # https://github.com/jstacoder/graphene-disable-introspection-middleware
     if is_introspection_disabled() and info.field_name.lower() in ['__schema', '__introspection']:
-        raise IntrospectionDisabled("Introspection is disabled")
+        raise IntrospectionDisabled(frappe._("Introspection is disabled"))
 
     return next_resolver(obj, info, **kwargs)
 
 
 def is_introspection_disabled():
-    return not frappe.local.conf.get("developer_mode") and \
-        not frappe.local.conf.get("enable_introspection_in_production")
+    return not cint(frappe.local.conf.get("developer_mode")) and \
+        not cint(frappe.local.conf.get("enable_introspection_in_production"))
