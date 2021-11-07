2d12802fa2c679477d25bbda534fad5cba4e7661
Elton Lobo <elton1231@gmail.com>
2021-11-07 17:43:44 +0400

    refactor: cache schema per site

--- frappe_graphql/utils/loader.py
+++ frappe_graphql/utils/loader.py
@@ -8,20 +8,20 @@ from graphql.error import GraphQLSyntaxError
 
 from .exceptions import GraphQLFileSyntaxError
 
-
-graphql_schema = None
+graphql_schemas = None
 
 
 def get_schema():
-    global graphql_schema
+    global graphql_schemas
 
-    if graphql_schema is not None:
-        return graphql_schema
-
+    if graphql_schemas is not None and isinstance(graphql_schemas, dict) and frappe.local.site in graphql_schemas:
+        return graphql_schemas.get(frappe.local.site)
+    if not graphql_schemas:
+        graphql_schemas = {}
     schema = graphql.build_schema(get_typedefs())
     execute_schema_processors(schema=schema)
 
-    graphql_schema = schema
+    graphql_schemas[frappe.local.site] = schema
     return schema
 
 
