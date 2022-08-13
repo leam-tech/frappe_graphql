bd2a4a138587607f08757e710a6898f0fd116463
Fahim Ali Zain <fahimalizain@gmail.com>
2022-08-13 14:26:02 +0530

    fix: removed unused code

--- frappe_graphql/__init__.py
+++ frappe_graphql/__init__.py
@@ -1,24 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-import time
-
-
-def profile_fn(fn):
-    from graphql import GraphQLResolveInfo
-
-    def _inner(*args, **kwargs):
-        _v = fn.__name__
-        if len(args) > 1 and isinstance(args[1], GraphQLResolveInfo):
-            _v += f" df: {args[1].field_name}"
-
-        t = time.perf_counter()
-        v = fn(*args, **kwargs)
-        print(_v, f"{(time.perf_counter() - t) * 1000}ms")
-        return v
-
-    return _inner
-
-
 from .utils.cursor_pagination import CursorPaginator  # noqa
 from .utils.loader import get_schema  # noqa
 from .utils.exceptions import ERROR_CODED_EXCEPTIONS, GQLExecutionUserError, GQLExecutionUserErrorMultiple  # noqa
