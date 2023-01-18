aa32f63a12f918f018bda45fa84dc291fc2cc82e
Elton Lobo <elton1231@gmail.com>
2023-01-18 16:37:31 +0400

    feat: get fields selected from child table query

--- frappe_graphql/utils/resolver/child_tables.py
+++ frappe_graphql/utils/resolver/child_tables.py
@@ -4,6 +4,9 @@ from frappe.model.meta import Meta
 
 from .dataloaders import get_child_table_loader
 from .utils import get_frappe_df_from_resolve_info
+from ..depth_limit_validator import is_introspection_key
+from ..extract_requested_fields_resolver_info import get_fields
+from ..permissions import get_allowed_fieldnames_for_doctype
 
 
 def setup_child_table_resolvers(meta: Meta, gql_type: GraphQLType):
@@ -25,8 +28,35 @@ def _child_table_resolver(obj, info: GraphQLResolveInfo, **kwargs):
     if not df:
         return []
 
+    key = _get_child_table_path_key(info)
+    valid_fields = info.context.get(key)
+
+    if not info.context.get(key):
+        valid_fields = _get_fields_child_table(info, df.options, df.parent)
+        info.context[key] = valid_fields
+
     return get_child_table_loader(
         child_doctype=df.options,
         parent_doctype=df.parent,
-        parentfield=df.fieldname
+        parentfield=df.fieldname,
+        fields=valid_fields
     ).load(obj.get("name"))
+
+
+def _get_child_table_path_key(info):
+    return "-".join([p for p in info.path.as_list() if isinstance(p, str)])
+
+
+def _get_fields_child_table(info: GraphQLResolveInfo, child_doctype: str, parent_doctype: str):
+    selected_fields = {
+        key.replace('__name', '')
+        for key in get_fields(info).keys()
+        if not is_introspection_key(key)
+    }
+    # we need to make sure parent is there
+    selected_fields.add("parent")
+    fieldnames = set(get_allowed_fieldnames_for_doctype(
+        doctype=child_doctype,
+        parent_doctype=parent_doctype
+    ))
+    return list(set(list(selected_fields.intersection(fieldnames))))
